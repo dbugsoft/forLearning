aaa7d26c3c9de281fd2143f57edb7b66ea0e6685
binitabashyal27 <142423058+binitabashyal27@users.noreply.github.com>
2023-08-22 13:10:47 +0545

    my first commit:I created Components folder under src.I added font awesome in index.html.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import Form from './Components/Form';
+import Todolist from './Components/Todolist';
 
 function App() {
+  const [todos,setTodos] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <h1>ToDoList-App</h1>
+        <div><Form todos={todos} setTodos={setTodos}/></div>
+        <div><Todolist todos={todos} setTodos={setTodos}/></div>
+      </div>
     </div>
   );
 }
